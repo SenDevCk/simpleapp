c82024deed12e07a35a91f692265c43f82368891
SenDevCk <chandan101288@gmail.com>
2026-04-10 16:26:26 +0530

    first commit

---
+++ jenkins/src/main/java/com/chandan/jenkins/ctrl/HomeController.java
@@ -0,0 +1,16 @@
+package com.chandan.jenkins.ctrl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello from Spring Boot + Thymeleaf!");
+        model.addAttribute("name", "User");
+        return "index";
+    }
+}
